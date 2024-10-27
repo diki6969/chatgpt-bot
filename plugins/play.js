9a4c6edfe1f2a233a0b3095e11441437434291b4
IkyyOfficial <gendonmenjeng@gmail.com>
2024-10-28 00:47:32 +0700

    update plugins/play.js

--- plugins/play.js
+++ plugins/play.js
@@ -5,21 +5,36 @@ module.exports = async (m, out, kyy, a) => {
     kyy.wait(m.key.remoteJid, a.key);
     let search = await yts(out.input);
     let f = search.all.filter(v => !v.url.includes("@"));
-    let res = await Api.widipe("download/ytdl", {
-        url: f[0].url
-    });
-    if (!res.status) return;
-    let anu = res.result;
+    let anu = f[0];
+    const pay = {
+        videoid: anu.videoId,
+        downtype: "mp3",
+        vquality: 144
+    };
+
+    const ft = (
+        await axios.post("https://api-cdn.saveservall.xyz/ajax-v2.php", pay, {
+            headers: {
+                accept: "*/*",
+                "content-type":
+                    "application/x-www-form-urlencoded; charset=UTF-8",
+                Origin: "https://api-cdn.saveservall.xyz",
+                Referer: `https://api-cdn.saveservall.xyz/?videoId=${anu.videoId}`,
+                "User-Agent": "IkyyOFC",
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        })
+    ).data;
     await kyy.sendAudio(
         m.key.remoteJid,
         {
-            audio: { url: anu.mp3 },
+            audio: { url: ft.url },
             mimetype: "audio/mpeg",
             fileName: `${anu.title}.mp3`
         },
         a,
         anu.title,
         anu.thumbnail,
-        f[0].url
+        anu.url
     );
 };
