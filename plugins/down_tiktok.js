9ab7c289cb297e99d7b5fb53e03e77de7f560c73
IkyyOfficial <gendonmenjeng@gmail.com>
2024-11-04 18:13:18 +0700

    update plugins/down_tiktok.js

--- plugins/down_tiktok.js
+++ plugins/down_tiktok.js
@@ -1,13 +1,12 @@
-const axios = require("axios");
 module.exports = async (m, out, kyy, a) => {
     kyy.wait(m.key.remoteJid, a.key);
-    let res = await Api.widipe("download/tiktokdl", { url: out.input });
+    let res = await Api.widipe("download/tikdl", {
+        url: out.input
+    });
     kyy.sendMessage(
         m.key.remoteJid,
         {
-            video: {
-                url: res.result.video
-            }
+            audio: { url: res.result.video[0] }
         },
         { quoted: a }
     );
