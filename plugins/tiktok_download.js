91fc1a722c982a7955e9c9c559540e49a74373d4
IkyyOfficial <gendonmenjeng@gmail.com>
2024-11-04 18:23:06 +0700

    update plugins/tiktok_download.js

--- plugins/tiktok_download.js
+++ plugins/tiktok_download.js
@@ -1,12 +1,12 @@
 module.exports = async (m, out, kyy, a) => {
     kyy.wait(m.key.remoteJid, a.key);
-    let res = await Api.widipe("download/tikdl", {
+    let response = await Api.widipe("download/tikdl", {
         url: out.input
     });
     kyy.sendMessage(
         m.key.remoteJid,
         {
-            video: { url: res.result.video[0] }
+            video: { url: response.result.video[0] }
         },
         { quoted: a }
     );
