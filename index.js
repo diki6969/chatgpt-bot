f5a0908f75bdf5a30f5c77c7cc95ccb1c909e8b3
IkyyOfficial <gendonmenjeng@gmail.com>
2024-11-04 17:43:54 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const a = require('./client');
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+process.on("uncaughtException", console.error);
+const a = require("./client");
 require("./server");
 require("./config");
-a.connect().catch(() => a.connect())
+a.connect().catch(() => a.connect());
