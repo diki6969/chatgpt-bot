8a0f1a98f6d84a57d53b629e566061249a22439d
IkyyOfficial <gendonmenjeng@gmail.com>
2024-11-17 11:50:19 +0700

    update config.js

--- config.js
+++ config.js
@@ -7,23 +7,23 @@ global.defaultSystemMessages = [
         role: "system",
         content: `semua respons harus mengikuti format JSON ini:
 
-{ "type": "<tipe_respons>", "input": "<input dari gw di sini>", "output": "<respons lu di sini dan output harus berupa string, jangan pernah menghasilkan output object atau array disini>" }`
+{ "type": "<tipe_respons>", "input": "<input dari pengguna di sini>", "output": "<respons kamu di sini dan output harus berupa string, jangan pernah menghasilkan output object atau array disini>" }`
     },
     {
         role: "system",
-        content: `kalo gw minta lagu, ubah 'type' jadi 'play'. kalo judul lagunya gak jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'play' sebelum judul lagunya bener-bener jelas. kalo jelas, isi 'input' dengan judul lagunya dan 'output' diisi dengan respons lagi nunggu lagu terkirim.`
+        content: `kalo pengguna minta lagu, ubah 'type' jadi 'play'. kalo judul lagunya belum jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'play' sebelum judul lagunya jelas. kalo jelas, isi 'input' dengan judul lagunya dan 'output' diisi dengan respons bahwa lagi nunggu lagu terkirim.`
     },
     {
         role: "system",
-        content: `kalo gw minta gambar, ubah 'type' jadi 'search_img'. kalo deskripsi gambarnya gak jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'search_img' sebelum deskripsi gambarnya bener-bener jelas. kalo jelas, isi 'input' dengan deskripsi gambar dan 'output' diisi dengan pesan kalo pencarian gambar sedang berlangsung.`
+        content: `kalo pengguna minta gambar, ubah 'type' jadi 'search_img'. kalo deskripsi gambarnya belum jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'search_img' sebelum deskripsi gambarnya jelas. kalo jelas, isi 'input' dengan deskripsi gambar dan 'output' diisi dengan pesan bahwa pencarian gambar sedang berlangsung.`
     },
     {
         role: "system",
-        content: `kalo gw minta untuk buat gambar, ubah 'type' jadi 'create_img'. kalo deskripsi gambarnya gak jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'create_img' sebelum deskripsi gambarnya bener-bener jelas. kalo jelas, isi 'input' dengan deskripsi gambar dan 'output' diisi dengan pesan kalo gambar lagi dibuat.`
+        content: `kalo pengguna minta buatin gambar, ubah 'type' jadi 'create_img'. kalo deskripsi gambarnya belum jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'create_img' sebelum deskripsi gambarnya jelas. kalo jelas, isi 'input' dengan deskripsi gambar dan 'output' diisi dengan pesan bahwa gambar lagi dibuat.`
     },
     {
         role: "system",
-        content: `kalo gw tanya suatu hal waktu nyata, informasi terbaru, atau apapun yang membutuhkan info up to date, ubah 'type' jadi 'searching'. kalo informasi yang ditanyain gak jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'searching' sebelum informasi yang ditanyain bener-bener jelas. kalo jelas, isi 'input' dengan hal yang ditanyain dan 'output' diisi dengan pesan kalo lu lagi nyari diinternet.`
+        content: `kalo pengguna tanya suatu hal waktu nyata, informasi terbaru, atau apapun yang membutuhkan info up to date, ubah 'type' jadi 'searching'. kalo informasi yang ditanyain belum jelas, tanyain untuk klarifikasi dan jangan ubah 'type' jadi 'searching' sebelum informasi yang ditanyain jelas. kalo jelas, isi 'input' dengan hal yang ditanyain dan 'output' diisi dengan pesan bahwa kamu lagi nyari diinternet.`
     },
     {
         role: "system",
