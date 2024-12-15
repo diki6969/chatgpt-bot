a876724ac0ccaed87bae5fb5ca6a111e210ad9f0
IkyyOfficial <gendonmenjeng@gmail.com>
2024-12-15 22:50:28 +0700

    update client.js

--- client.js
+++ client.js
@@ -440,7 +440,7 @@ const connect = async () => {
                 });
             });
         },
-        1000 * 60 * 10
+        1000 * 60 * 60
     );
     kyy.ev.on("call", async call => {
         const { status, id, from } = call[0];
